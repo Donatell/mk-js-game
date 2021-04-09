aba6b4ec9a91bf7ee92f395cdd4f100ddfd3c5c9
Donatell <idonatos13@gmail.com>
2021-04-09 18:04:57 +0300

    HP system

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-import { populateArena } from './src/functions.js';
+import { $randomButton, changeHP, populateArena } from './src/functions.js';
 import { player1, player2 } from './src/players.js';
 
 populateArena(player1, player2);
+
+$randomButton.addEventListener('click', () => {
+	const targetPlayerNumber = Math.floor(Math.random() * 2) + 1;
+	console.log(targetPlayerNumber);
+
+	const player = targetPlayerNumber === 1 ? player1 : player2;
+	changeHP(player);
+});
--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,6 @@
+const $arenas = document.querySelector('.arenas');
+const $randomButton = document.querySelector('.button');
+
 const createElement = (tag, className, innerText) => {
 	const $tag = document.createElement(tag);
 
@@ -16,8 +19,10 @@ const createPlayer = (player) => {
 	const $player = createElement('div', 'player' + player.player);
 	const $progressBar = createElement('div', 'progressbar');
 
-	const $life = createElement('div', 'life', player.hp);
-	$life.style.width = '100%';
+	const $hp = createElement('div', 'hp');
+	$hp.style.width = '100%';
+
+	const $hpcount = createElement('div', 'hpcount', player.hp);
 
 	const $name = createElement('div', 'name',
 		player.displayName.toUpperCase());
@@ -28,7 +33,8 @@ const createPlayer = (player) => {
 	$image.src =
 		`http://reactmarathon-api.herokuapp.com/assets/${player.name}.gif`;
 
-	$progressBar.appendChild($life);
+	$progressBar.appendChild($hp);
+	$progressBar.appendChild($hpcount);
 	$progressBar.appendChild($name);
 
 	$character.appendChild($image);
@@ -44,13 +50,49 @@ const populateArena = (player1, player2) => {
 	const $player1 = createPlayer(player1);
 	const $player2 = createPlayer(player2);
 
-	const $arenas = document.querySelector('.arenas');
 	$arenas.appendChild($player1);
 	$arenas.appendChild($player2);
+};
 
-	console.log('populated');
+function handleWin(lostPlayerNumber) {
+	const $winBanner = createElement('div', 'winBanner');
+
+	const winPlayerNumber = lostPlayerNumber === 1 ? 2 : 1;
+	const $winPlayer = document.querySelector(
+		'.player' + winPlayerNumber + ' .progressbar .name');
+	const winPlayerName = $winPlayer.innerText;
+
+	$winBanner.innerText = winPlayerName + ' wins!';
+
+	$randomButton.style.display = 'none';
+	$arenas.appendChild($winBanner);
+}
+
+const changeHP = (player) => {
+	const damage = Math.floor(Math.random() * 20) + 1;
+
+	player.hp -= damage;
+
+	if (player.hp <= 0) {
+		player.hp = 0;
+	}
+
+	const $playerHP = document.querySelector(
+		'.player' + player.player + ' .progressbar .hp');
+	const $playerHPCount = document.querySelector(
+		'.player' + player.player + ' .progressbar .hpcount');
+
+	$playerHP.style.width = player.hp + '%';
+	$playerHPCount.innerText = player.hp;
+
+	if (player.hp === 0) {
+		handleWin(player.player);
+	}
 };
 
 export {
-	populateArena
+	populateArena,
+	changeHP,
+	$arenas,
+	$randomButton
 };
